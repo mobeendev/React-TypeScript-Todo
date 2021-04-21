6545c4ce52bb7157846bf85953ced0cce4160757
Abdul Mobeen <mobeendev@gmail.com>
2021-04-21 21:31:35 +0300

    todo list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoListItem } from './TodoListItem';
+import { TodoList } from './TodoList';
+
+const initialTodos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+  {
+    text: 'Some Event',
+    complete: false,
+  },
+  {
+    text: 'Go to bazar',
+    complete: true,
+  },
+];
 
 function App() {
-  return <>Hello World</>;
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return <TodoList todos={todos} toggleTodo={toggleTodo} />;
+
 }
 
 export default App;
